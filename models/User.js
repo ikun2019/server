0022aee5dd98df2fc4f8af2abecf13615e75e61f
ikun <0215.miyatto@gmail.com>
2023-04-03 19:26:48 +0900

    userモデルにJWT署名メソッドとパスワードのハッシュ化機能の追加

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = sequelize.define('user', {
   id: {
@@ -15,7 +17,25 @@ const User = sequelize.define('user', {
   email: {
     type: DataTypes.STRING,
     allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
   }
+});
+
+// JWT署名
+User.prototype.getSignedJwtToken = function () {
+  return jwt.sign(
+    { id: this.id },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRE }
+  );
+};
+// パスワードのハッシュ化
+User.beforeCreate(async (user) => {
+  const salt = await bcrypt.getSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 });
 
 module.exports = User;
