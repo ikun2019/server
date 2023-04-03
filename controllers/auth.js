7d87dc3b7ad2b90a19638419783866b1d014e5af
ikun <0215.miyatto@gmail.com>
2023-04-04 04:21:51 +0900

    ログイン機能の実装

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,12 +16,25 @@ exports.getLogin = async (req, res, next) => {
 // 機能
 exports.postLogin = async (req, res, next) => {
   try {
-    const email = req.body.email;
-    const password = req.body.password;
-
+    const foundUser = await User.findOne({ where: { email: req.body.email } });
+    if (!foundUser) {
+      return res.status(403).json({
+        success: false,
+        message: 'ユーザーが見つかりません'
+      });
+    }
+    const isMatch = await foundUser.comparePassword(req.body.password);
+    if (!isMatch) {
+      return res.status(403).json({
+        success: false,
+        message: 'パスワードが一致しません'
+      });
+    }
+    const token = foundUser.getSignedJwtToken();
     req.isLoggedIn = true;
     res.status(200).json({
-      success: true
+      success: true,
+      token
     });
   } catch (err) {
     res.status(500).json({
