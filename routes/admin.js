fe8f6784c7dbb63526bca5bc65b37dc4f1799db2
ikun <0215.miyatto@gmail.com>
2023-04-15 13:12:59 +0900

    add-productにvalidationを設定

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,25 @@
 const router = require('express').Router();
 const adminController = require('../controllers/admin');
 const verifyToken = require('../middlewares/verify-token');
+const { check, body } = require('express-validator');
 
 // * 商品追加ページ => /api/admin/add-product
 // UI表示 => GET
 router.get('/add-product', verifyToken, adminController.getAddProduct);
 // 機能 => POST
-router.post('/add-product', verifyToken, adminController.postAddProduct);
+router.post('/add-product',
+  verifyToken,
+  [
+    body('title')
+      .isLength({ min: 3 })
+      .trim(),
+    body('price')
+      .isFloat(),
+    body('description')
+      .isLength({ min: 8, max: 400 })
+      .trim()
+  ],
+  adminController.postAddProduct);
 
 // * 商品一覧表示 => /api/admin/products
 // UI表示 GET
