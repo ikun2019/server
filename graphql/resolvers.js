e3011498e7c77a9dd960451aec2d3b55186cab2a
ikun <0215.miyatto@gmail.com>
2023-04-22 17:30:25 +0900

    バリデーション設定

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,22 @@
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
 const User = require('../models/User');
 
 module.exports = {
   createUser: async function (args, req) {
+    const errors = [];
+    if (!validator.isEmail(args.userInput.email)) {
+      errors.push({ message: "emailが空です" });
+    }
+    if (validator.isEmpty(args.userInput.password) || !validator.isLength(args.userInput.password, { min: 5 })) {
+      errors.push({ message: 'パスワードが短いです' });
+    }
+    if (errors.length > 0) {
+      const error = new Error('Invalid Input');
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
     const existingUser = await User.findOne({
       where: { email: args.userInput.email }
     });
